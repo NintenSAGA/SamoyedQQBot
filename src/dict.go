03bd02af92ca2bd610884d9c92908bea568938ea
Ziyue Tan <201250093@smail.nju.edu.cn>
2023-08-26 17:00:55 +0800

    feat: basic functions

---
+++ src/dict.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/armon/go-radix"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+const (
+	DICT_API = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+)
+
+type Dict struct {
+	dictTree *radix.Tree
+	Current  string
+}
+
+func createDict() *Dict {
+	dict := Dict{}
+	dict.initDict()
+	return &dict
+}
+
+func (d *Dict) Reset() {
+	d.initDict()
+}
+
+func (d *Dict) initDict() {
+	wordlistPath := os.Getenv("WORDLIST_PATH")
+	if len(wordlistPath) == 0 {
+		log.Panicln("Missing env WORDLIST_PATH")
+	}
+
+	fd, err := os.Open(wordlistPath)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer fd.Close()
+
+	tree := radix.New()
+
+	scanner := bufio.NewScanner(fd)
+	for scanner.Scan() {
+		word := scanner.Text()
+		word = strings.TrimSpace(word)
+		word = strings.ToLower(word)
+		if len(word) <= 1 {
+			tree.Insert(word, false)
+		} else {
+			tree.Insert(word, true)
+		}
+	}
+	d.dictTree = tree
+	d.Current = ""
+}
+
+func (d *Dict) CheckWord(word string) (bool, error) {
+	reqUrl, _ := url.JoinPath(DICT_API, word)
+	resp, err := http.Get(reqUrl)
+	if err != nil {
+		log.Println(err.Error())
+		return false, fmt.Errorf("小狗脑子转不过来了")
+	}
+
+	if resp.StatusCode != 200 {
+		return false, fmt.Errorf("找不到这个词")
+	}
+	var unmarked bool
+	if val, ex := d.dictTree.Get(word); ex {
+		unmarked = val.(bool)
+	} else {
+		unmarked = true
+	}
+
+	if !unmarked {
+		return false, fmt.Errorf("之前说过了")
+	}
+
+	if len(d.Current) != 0 {
+		return strings.EqualFold(word[0:1], d.Current[len(d.Current)-1:]), fmt.Errorf("字母接不上")
+	}
+
+	return true, nil
+}
+
+func (d *Dict) markWord(word string) {
+	d.dictTree.Insert(word, false)
+}
+
+func (d *Dict) getMatchingWord(curWord string) (result string, existed bool) {
+	existed = false
+	d.markWord(curWord)
+	l := len(curWord)
+	last := curWord[l-1:]
+	last = strings.ToLower(last)
+	d.dictTree.WalkPrefix(last, func(key string, value interface{}) bool {
+		log.Println(key)
+		if !(value.(bool)) {
+			return false
+		}
+		result = key
+		existed = true
+		return true
+	})
+
+	if existed {
+		d.markWord(result)
+		d.Current = result
+	}
+
+	return
+}
--- src/solver.go
+++ src/solver.go
@@ -4,30 +4,66 @@ import (
 	"fmt"
 	"github.com/huichen/sego"
 	"log"
-	"math/rand"
 	"os"
+	"strings"
 )
 
 type Solver struct {
-	seg *sego.Segmenter
+	seg  *sego.Segmenter
+	dict *Dict
 }
 
 func createSolver() *Solver {
+	return &Solver{
+		seg:  createSeg(),
+		dict: createDict(),
+	}
+}
+
+func createSeg() *sego.Segmenter {
 	seg := sego.Segmenter{}
 	dictPath := os.Getenv("DICT_PATH")
 	if len(dictPath) == 0 {
 		log.Panicln("Missing env DICT_PATH")
 	}
 	seg.LoadDictionary(dictPath)
-	return &Solver{
-		seg: &seg,
-	}
+	return &seg
 }
 
 func (s *Solver) generateAnswer(username string, content string) string {
-	words := s.seg.Segment([]byte(content))
-	log.Println(sego.SegmentsToString(words, true))
-	idx := rand.Intn(len(words))
-	result := fmt.Sprintf("%v 你好呀，我觉得你说的这句话中，\"%v\"最有道理", username, words[idx].Token().Text())
-	return result
+	if len(content) != 0 && isPureLetter(content) {
+		if existed, err := s.dict.CheckWord(content); !existed {
+			return fmt.Sprintf("汪汪，小狗觉得‘%v’这个词不太对呢！好像是因为%v！当前单词：‘%v’", content, err.Error(), s.dict.Current)
+		}
+
+		result, existed := s.dict.getMatchingWord(content)
+		if !existed {
+			s.dict.Reset()
+			return fmt.Sprintf("小狗找不到了！小狗投降！我们重来吧！")
+		}
+
+		return result
+	} else {
+		switch {
+		case strings.HasPrefix(content, "重"):
+			s.dict.Reset()
+			return "汪汪好的！现在重新开始游戏～～"
+		default:
+			return fmt.Sprintf("汪汪，怎么了！？\n我是会玩英语单词接龙的小狗！\n" +
+				"* 如果你要开始接龙，请输入一个单词！\n" +
+				"* 如果要重开，那就告诉我“重来”！\n" +
+				"* 对了，找我的时候不要忘记 @我 哦！")
+
+		}
+
+	}
+}
+
+func isPureLetter(s string) bool {
+	for _, r := range s {
+		if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') {
+			return false
+		}
+	}
+	return true
 }
